73af41d88fbb0aa6cd45285c3b767175ae8f7ba6
BoringCrypto <b@rtje.net>
2021-07-08 16:34:29 +1000

    More formatting

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from './pages/Home.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from "./pages/Home.vue"
 
-const routes = [
-    { path: '/', component: Home },
-]
+const routes = [{ path: "/", component: Home }]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
 })
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).use(router).mount("#app")
